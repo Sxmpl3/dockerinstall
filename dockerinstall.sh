0d6d851c76c66817dd7d566958c5e6d1965eee9a
Izan Cano González <116309678+Sxmpl3@users.noreply.github.com>
2023-02-22 14:14:21 +0100

    Update dockerinstall.sh

--- dockerinstall.sh
+++ dockerinstall.sh
@@ -60,7 +60,7 @@ if [[ $1 == "-l" ]];then
     
    echo -e "\n[-] Iniciando contenedor de Login en el puerto 8080..."
    echo -e ""
-   docker run --name register -d -p 8080:80 sxmpl3/login-app > /dev/null 2>&1
+   docker run --name login -d -p 8080:80 -e PHP_EXTENSION=mysqli sxmpl3/login-app > /dev/null 2>&1
    echo -e "\n[+] Iniciado con éxito"
    echo -e ""
 fi 
@@ -69,7 +69,7 @@ if [[ $1 == "-r" ]];then
 
    echo -e "\n[-] Iniciando contenedor de Register en el puerto 8081..."
    echo -e ""
-   docker run --name register -d -p 8081:80 sxmpl3/register-app > /dev/null 2>&1
+   docker run --name register -d -p 8081:80 -e PHP_EXTENSION=mysqli sxmpl3/register-app > /dev/null 2>&1
    echo -e "\n[+] Iniciado con éxito"
    echo -e ""
 fi
