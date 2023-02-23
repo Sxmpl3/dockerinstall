f0f8e7f5bf1210f8a156b745d12b906af66b455b
Izan Cano González <116309678+Sxmpl3@users.noreply.github.com>
2023-02-23 01:17:46 +0100

    Delete init.sql

--- init.sql
+++
@@ -1,9 +0,0 @@
-USE credentials;
-CREATE TABLE users (
-  id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  username VARCHAR(30) NOT NULL,
-  password VARCHAR(30) NOT NULL
-);
-INSERT INTO users (username, password) VALUES
-  ('usuario1', 'clave1'),
-  ('usuario2', 'clave2');
